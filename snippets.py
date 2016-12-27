db9be2a6ae2655ba78f44c58f03cfb25d953ffcf
caboe2012 <chad.aboe@gmail.com>
2016-12-27 14:15:24 +0000

    Added get and put

--- snippets.py
+++ snippets.py
@@ -1,4 +1,20 @@
 import logging
 
-# set the log output file, and the log level
-logging.basicConfig(filename="snippets.log", level = logging.DEBUG)
+#set the log file name and level
+logging.basicConfig(filename = "snippets.log", logging.level = DEBUG)
+
+def put(name, snippet):
+    """
+    Store a snippet with the associated name.
+    Returns the name and the snippet.
+    """
+    logging.error("FIXME - Unimplemented put({!r}, {!r})".format("name, snippt))
+    return name, snippet
+
+def get(name):
+    """ Retrieve the snippet with the given name.
+    If there is no such snippet, return '404: Snippet not Found".
+    Returns the snippet.
+    """
+    logging.error("FIXME - Unimplemented - get({!r})".format(name))
+    return ""
