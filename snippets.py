1afb2858c5ad50a3828339e31d4d1cc12fa85f3e
caboe2012 <chad.aboe@gmail.com>
2016-12-28 16:19:51 +0000

    Finished put parser

--- snippets.py
+++ snippets.py
@@ -1,24 +1,27 @@
 import logging
 import argparse
 
-#set the log file name and level
-logging.basicConfig(filename = "snippets.log", level = logging.DEBUG)
+# set the log file name and the logging level
+logging.basicConfig(filname = "snippets.log", level = logging.DEBUG)
 
+
+# create the four CRUD functions for snippets skeleton
 def put(name, snippet):
     """
-    Store a snippet with the associated name.
-    Returns the name and the snippet.
+    STORE a snippet with the associated name.
+
+    RETURNS the name and the snippet
     """
-    logging.error("FIXME - Unimplemented put({!r}, {!r})".format(name, snippet))
+    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
     return name, snippet
 
 def get(name):
-    """ Retrieve the snippet with the given name.
-    If there is no such snippet, return '404: Snippet not Found".
-    Returns the snippet.
+    """ Retrieve the snippet with a given name.
+    If there is no such snippet, return '404: Snippet Not Found'.
+    Returns the Snippet.
     """
-    logging.error("FIXME - Unimplemented - get({!r})".format(name))
-    return ""
+    logging.error("FIXME: Unimplemented - get({!r})".format(name))
+    return '404: Snippet Not Found'
 
 def update(name, snippet):
     """ Modify the snippet with the given name."""
@@ -32,13 +35,19 @@ def delete(name, snippet):
 def main():
     """ Main Function """
     logging.info("Constructing Parser")
-    parser = argparse.ArgumentParser(description = "Store and retrieve snippets of text".)
-
-    subparsers = parser.add_subparsers(dest = "command", help = "Available commands")
+    parser = argparse.ArgumentParser(description = "Store and retrieve snippets of text.")
     
-    # Subparser for the put command
+    subparsers = parser.add_subparsers(dest = "command", help = "Available Commands")
+    
+    #Subparser for the put command
     logging.debug("Constructing put subparser")
-    put_parser = subparsers.add_parser("put", help = "Store a snippet")
-
+    put_parser = subparsers.add_parser("put", help ="Store a snippet")
+    put_parser.add_argument("name", help = "Name of the snippet")
+    put_parser.add_argument("snippet", help = "Snippet text")
+    
     arguments = parser.parse_args()
-
+    
+if __name__ == "__main__":
+    main()
+    
+    
